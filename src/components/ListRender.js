cd2c132bb92c3ef98d8c5bc0b1288144e2892c08
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-02-15 15:48:08 -0300

    Propriedade key

--- src/components/ListRender.js
+++ src/components/ListRender.js
@@ -3,12 +3,22 @@ import { useState } from "react";
 const ListRender = () => {
 
     const [list] = useState(['João', 'Lucas', 'José']);
+    const [users] = useState([
+        {id: 1 , name: 'nome 1'},
+        {id: 2 , name: 'nome 2'},
+        {id: 3 , name: 'nome 3'},
 
+    ])
     return (
         <div>
             <ul>
                 {list.map((item, index) => (
                     <li key={index}>{item}</li>
+                ))}
+            </ul>
+            <ul>
+                {users.map((item) => (
+                    <li key={item.id}>{item.name}</li>
                 ))}
             </ul>
         </div>
