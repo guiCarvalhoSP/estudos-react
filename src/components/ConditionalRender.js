5db63d2b0e3a71e376946fe64184f83b04a4431d
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-02-16 14:01:35 -0300

    If ternário

--- src/components/ConditionalRender.js
+++ src/components/ConditionalRender.js
@@ -4,11 +4,24 @@ const ConditionalRender = () => {
 
     const [x] = useState(true);
 
+    const [name] = useState("João");
+
     return (
         <div>
             <h1>Isso será exibido?</h1>
             {x && <p>Se x for true, sim</p>}
             {!x && <p>Agora x é falso</p>}
+
+            <h1>If ternário</h1>
+            {name === 'João' ? (
+            <div>
+                <p>Nome é joão</p>
+            </div>
+            ) : (
+                <div>
+                    <p>Nome não encontrado</p>
+                </div>
+                )}
         </div>
     )
 }
