8decf03b048f8b0c137f5bfaccc9923bf716d769
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-02-23 09:55:08 -0300

    Classes dinamicas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
+
+  let redTitle = true;
+
   return (
     <div className="App">
       <h1>React com CSS</h1>
       <MyComponent />
 
       <p>Paragrafo do app js</p>
+
+      <h2 className={ redTitle ? 'red-title' : 'title'}>Este titulo vai ter classe dinamica</h2>
     </div>
   );
 }
