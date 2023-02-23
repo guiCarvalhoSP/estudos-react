6cf076b275b8d8238659fd6f767f713431eb3947
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-02-23 11:12:39 -0300

    Simplificação de manipulação de state

---
+++ src/components/MyForm.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './MyForm.css'
+
+const MyForm = () => {
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+    return (
+        <div>
+            <form>
+                <div>
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" name='name' placeholder='Digite seu nome' onChange={handleName} />
+                </div>
+                <label>
+                    <span>E-mail</span>
+                    <input type="email" name='email' placeholder='Digite seu email' onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                <input type="submit" value='enviar' />
+            </form>
+        </div>
+    )
+}
+
+export default MyForm
