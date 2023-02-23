3a34a6a2c64cf1d44a0013956b2f581c69889da8
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-02-23 11:25:34 -0300

    Controlled inputs

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,29 +1,46 @@
-import { useState } from 'react'
-import './MyForm.css'
+import { useState } from "react";
+import "./MyForm.css";
 
-const MyForm = () => {
+const MyForm = ({ user }) => {
+  const [name, setName] = useState(user ? user.name : "");
+  const [email, setEmail] = useState(user ? user.email : "");
 
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
 
-    const handleName = (e) => {
-        setName(e.target.value);
-    }
-    return (
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviando formulario");
+    console.log(name, email);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <div>
-            <form>
-                <div>
-                    <label htmlFor="name">Nome:</label>
-                    <input type="text" name='name' placeholder='Digite seu nome' onChange={handleName} />
-                </div>
-                <label>
-                    <span>E-mail</span>
-                    <input type="email" name='email' placeholder='Digite seu email' onChange={(e) => setEmail(e.target.value)} />
-                </label>
-                <input type="submit" value='enviar' />
-            </form>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome"
+            onChange={handleName}
+            value={name}
+          />
         </div>
-    )
-}
+        <label>
+          <span>E-mail</span>
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite seu email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <input type="submit" value="enviar" />
+      </form>
+    </div>
+  );
+};
 
-export default MyForm
+export default MyForm;
