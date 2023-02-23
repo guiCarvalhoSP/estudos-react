a83ecaf8e1832b24dbc7f1ceffd86f2b9b4bf938
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-02-23 09:49:17 -0300

    css

---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import './MyComponent.css';
+
+const MyComponent = () => {
+  return (
+    <div>
+        <p className='my-comp-paragraph'>Teste de paragrafo</p>
+    </div>
+  )
+}
+
+export default MyComponent
