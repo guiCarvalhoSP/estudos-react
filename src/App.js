031ae45c5a9cd21c82b42050c54c024f171a9004
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-02-28 14:41:01 -0300

    Loading e tratamento de erro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import { useFetch } from './hooks/useFetch';
 
@@ -10,7 +10,7 @@ function App() {
 
   const [products, setProducts] = useState([]);
 
-  const { data: items } = useFetch(url);
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
@@ -20,7 +20,7 @@ function App() {
   //     const res = await fetch(url);
 
   //     const data = await res.json();
-  
+
   //     setProducts(data);
   //   }
   //   fetchData();
@@ -35,16 +35,18 @@ function App() {
       price
     };
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(product),
-    });
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(product),
+    // });
 
-    const addedProduct = await res.json();
-    setProducts((previousProducts) => [...previousProducts, addedProduct]);
+    // const addedProduct = await res.json();
+    // setProducts((previousProducts) => [...previousProducts, addedProduct]);
+
+    httpConfig(product, "POST");
 
     setName('');
     setPrice('')
@@ -53,35 +55,41 @@ function App() {
   return (
     <div className="App">
       <h1>Lista de produtos:</h1>
-      <ul>
-        {items && items.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
-        ))}
-      </ul>
+
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      {!loading && (
+        <ul>
+          {items && items.map((product) => (
+            <li key={product.id}>{product.name} - R$: {product.price}</li>
+          ))}
+        </ul>
+      )}
+
       <div className='add-product'>
-          <form onSubmit={handleSubmit}>
-            <label>
-              Nome:
-              <input 
-                type="text" 
-                value={name} 
-                name='name' 
-                onChange={(e) => setName(e.target.value)}
-              />
-            </label>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name='name'
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
 
-            <label>
-              Preço:
-              <input 
-                type="number" 
-                value={price} 
-                name='price' 
-                onChange={(e) => setPrice(e.target.value)}
-              />
-            </label>
-
-            <input type="submit" value='criar'/>
-          </form>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name='price'
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          {loading && <input type="submit" disabled value='Aguarde' />}
+          {!loading && <input type="submit" value='criar' />}
+        </form>
       </div>
     </div>
   );
